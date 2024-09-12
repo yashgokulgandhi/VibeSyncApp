0fea6861ddf9a8ea04a11249fb1f7103c193f42f
YashGokulgandhi <yashgokulgandhi6@gmail.com>
2024-09-13 01:18:01 +0530

    google login completed

--- app/src/main/java/com/example/vibesyncapp/LoginActivity.java
+++ app/src/main/java/com/example/vibesyncapp/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.example.vibesyncapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,20 +18,28 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText emailInput,passwordInput;
-    Button Loginbtn;
+    Button Loginbtn,googlelogin;
     TextView clickRegister,recoverpassword;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
+    int REQ_ONE_TAP=20;
 
     @Override
     public void onStart() {
@@ -52,6 +63,16 @@ public class LoginActivity extends AppCompatActivity {
         mAuth=FirebaseAuth.getInstance();
         progressBar=findViewById(R.id.progressbar);
         recoverpassword=findViewById(R.id.recoverpassword);
+        googlelogin=findViewById(R.id.loginwithgoogle);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+
+        GoogleSignInClient oneTapClient = GoogleSignIn.getClient(this, gso);
+
+
 
         clickRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +91,8 @@ public class LoginActivity extends AppCompatActivity {
                 LinearLayout linearLayout=new LinearLayout(LoginActivity.this);
                 EditText emailtext=new EditText(LoginActivity.this);
                 emailtext.setHint("Enter Email");
+                emailtext.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+                emailtext.setEms(20);
 
                 linearLayout.addView(emailtext);
                 linearLayout.setPadding(10,10,10,10);
@@ -80,6 +103,7 @@ public class LoginActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
 
                         String email=emailtext.getText().toString().trim();
+
                         mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
@@ -155,7 +179,51 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        googlelogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = oneTapClient.getSignInIntent();
+
+                startActivityForResult(signInIntent, REQ_ONE_TAP);
+
+
+            }
+        });
+
 
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ_ONE_TAP) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                String idToken = account.getIdToken();
+                if (idToken != null) {
+                    AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(idToken, null);
+                    mAuth.signInWithCredential(firebaseCredential)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign-in success
+                                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                    } else {
+                                        // Sign-in failed
+                                        Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            } catch (ApiException e) {
+                Toast.makeText(this, "Sign in failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/vibesyncapp/MainActivity.java
+++ app/src/main/java/com/example/vibesyncapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vibesyncapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,6 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -18,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     Button logout;
     FirebaseAuth auth;
     FirebaseUser user;
+
+    GoogleSignInClient googleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
         user= auth.getCurrentUser();
         userText=findViewById(R.id.username);
         logout=findViewById(R.id.logout);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
 
         if(user==null)
         {
@@ -42,8 +57,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 auth.signOut();
-                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
-                startActivity(intent);
+
+                googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // Redirect to LoginActivity after sign out
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear activity stack
+                        startActivity(intent);
+                        finish(); // Close MainActivity
+                    }
+                });
             }
         });
 
